73f4409879fa782d7f6e74deeb118adf861ca4b3
Afonso Hormigo Queiroz <qafonso70@gmail.com>
2026-05-28 22:13:55 +0100

    fix(ci): not building with local changes in deploy script

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,8 @@ else
 fi
 
 git fetch origin main
-git reset --hard FETCH_HEAD
+git reset --hard origin/main
+git clean -fd
 git remote set-url origin "https://github.com/INNERStudiosPT/hrm.git" || true
 
 echo "🏗️ Step 2: Rebuilding and restarting hrm-web container..."
